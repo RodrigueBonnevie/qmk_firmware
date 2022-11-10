a5abd58f6bf6ea8a42de5eab064aca49c6cd9505
RodrigueBonnevie <bonnevie@kth.se>
2022-11-10 22:43:58 +0100

    changed from normal to swedish keycodes

--- keyboards/crkbd/keymaps/combos/keymap.c
+++ keyboards/crkbd/keymaps/combos/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+#include <keymap_swedish.h>
 
   // Todo:
     // Combos
@@ -26,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     // Gaming layer?
     // There is something wrong with the blank keys and the custom keymaps, will have to look into this later// There is something wrong with the blank keys and the custom keymaps, will have to look into this later
   // Minor Todos:
-    // Two KC_MINS
+    // Two SE_MINS
     // Clean up the controls and modifiers that are laying around as doubles
     // Remove edge columns and inner thumbs
 
@@ -44,11 +45,11 @@ enum custom_keycodes{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LBRC,
+      XXXXXXX,    SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,   SE_P,  SE_LBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_SCLN,
+      XXXXXXX,    SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_QUOT, SE_SCLN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+      XXXXXXX,    SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,                         SE_N,    SE_M, SE_COMM,  SE_DOT, SE_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC,   MO(1),      MO(2), KC_LSFT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
@@ -56,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_BSLS, XXXXXXX,
+      XXXXXXX, SE_EXLM,   SE_AT, SE_HASH,  SE_DLR, SE_PERC,                      SE_CIRC, SE_AMPR, SE_LPRN, SE_RPRN, SE_BSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LGUI, KC_LCTL, KC_LALT,  KC_TAB, KC_LSFT,                      KC_MINS, KC_ASTR, KC_LCBR, KC_RCBR, KC_SCLN, XXXXXXX,
+      XXXXXXX, KC_LGUI, KC_LCTL, KC_LALT,  KC_TAB, KC_LSFT,                      SE_MINS, SE_ASTR, SE_LCBR, SE_RCBR, SE_SCLN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,                      KC_UNDS, KC_COLN, KC_LBRC, KC_RBRC, KC_PIPE, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SE_TILD,                      SE_UNDS, SE_COLN, SE_LBRC, SE_RBRC, SE_PIPE, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC, _______,      MO(3), KC_LSFT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
@@ -68,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9, KC_PLUS,                      XXXXXXX,  WIN_OH,  WIN_EH, WIN_EUH, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,    SE_7,    SE_8,    SE_9, SE_PLUS,                      XXXXXXX,  WIN_OH,  WIN_EH, WIN_EUH, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX,    KC_4,    KC_5,    KC_6,  KC_EQL,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,    SE_4,    SE_5,    SE_6,  SE_EQL,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_0,                       KC_DEL, KC_BSPC, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+      XXXXXXX, XXXXXXX,    SE_1,    SE_2,    SE_3,    SE_0,                       KC_DEL, KC_BSPC, SE_COMM,  SE_DOT, SE_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC,   MO(3),    _______, KC_LSFT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
@@ -105,11 +106,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Gaming layer
   [5] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LBRC,
+       KC_TAB,    SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,   SE_P,  SE_LBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_SCLN,
+      KC_LSFT,    SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_QUOT, SE_SCLN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   DF(0),
+      KC_LCTL,    SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,                         SE_N,    SE_M, SE_COMM,  SE_DOT, SE_SLSH,   DF(0),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC,   MO(2),    XXXXXXX, KC_LSFT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
@@ -117,9 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-const uint16_t PROGMEM combo_escape[]     = {KC_W, KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM combo_enter[]      = {KC_J, KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM combo_backspace[]  = {KC_U, KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_escape[]     = {SE_W, SE_E, SE_R, COMBO_END};
+const uint16_t PROGMEM combo_enter[]      = {SE_J, SE_K, SE_L, COMBO_END};
+const uint16_t PROGMEM combo_backspace[]  = {SE_U, SE_I, SE_O, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_escape,     KC_ESC),
@@ -127,55 +128,55 @@ combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_backspace,  KC_BSPC),
 };
 
-void tap_key(uint16_t keycode) {
-  register_code(keycode);
-  unregister_code(keycode);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch(keycode) {
-    case WIN_OH: // å and Å
-      if (record->event.pressed) {
-        uint8_t temp_mod = get_mods();
-        clear_mods();
-        register_code(KC_LALT);
-        if (temp_mod & MODS_SHIFT_MASK) {
-          tap_key(KC_P1); tap_key(KC_P4); tap_key(KC_P3); // Å
-        } else {
-          tap_key(KC_P1); tap_key(KC_P3); tap_key(KC_P4); // å
-        }
-        unregister_code(KC_LALT);
-        return false;
-      }
-      break;
-    case WIN_EH: // ä and Ä
-      if (record->event.pressed) {
-        uint8_t temp_mod = get_mods();
-        clear_mods();
-        register_code(KC_LALT);
-        if (temp_mod & MODS_SHIFT_MASK) {
-          tap_key(KC_P1); tap_key(KC_P4); tap_key(KC_P2); // Ä
-        } else {
-          tap_key(KC_P1); tap_key(KC_P3); tap_key(KC_P2); // ä
-        }
-        unregister_code(KC_LALT);
-        return false;
-      }
-      break;
-    case WIN_EUH: // ö and Ö
-      if (record->event.pressed) {
-        uint8_t temp_mod = get_mods();
-        clear_mods();
-        register_code(KC_LALT);
-        if (temp_mod & MODS_SHIFT_MASK) {
-          tap_key(KC_P1); tap_key(KC_P5); tap_key(KC_P3); // Ö
-        } else {
-          tap_key(KC_P1); tap_key(KC_P4); tap_key(KC_P8); // ö
-        }
-        unregister_code(KC_LALT);
-        return false;
-      }
-      break;
-  }
-  return true;
-}
+//void tap_key(uint16_t keycode) {
+//  register_code(keycode);
+//  unregister_code(keycode);
+//}
+//
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//  switch(keycode) {
+//    case WIN_OH: // å and Å
+//      if (record->event.pressed) {
+//        uint8_t temp_mod = get_mods();
+//        clear_mods();
+//        register_code(SE_LALT);
+//        if (temp_mod & MODS_SHIFT_MASK) {
+//          tap_key(SE_P1); tap_key(SE_P4); tap_key(SE_P3); // Å
+//        } else {
+//          tap_key(SE_P1); tap_key(SE_P3); tap_key(SE_P4); // å
+//        }
+//        unregister_code(SE_LALT);
+//        return false;
+//      }
+//      break;
+//    case WIN_EH: // ä and Ä
+//      if (record->event.pressed) {
+//        uint8_t temp_mod = get_mods();
+//        clear_mods();
+//        register_code(SE_LALT);
+//        if (temp_mod & MODS_SHIFT_MASK) {
+//          tap_key(SE_P1); tap_key(SE_P4); tap_key(SE_P2); // Ä
+//        } else {
+//          tap_key(SE_P1); tap_key(SE_P3); tap_key(SE_P2); // ä
+//        }
+//        unregister_code(SE_LALT);
+//        return false;
+//      }
+//      break;
+//    case WIN_EUH: // ö and Ö
+//      if (record->event.pressed) {
+//        uint8_t temp_mod = get_mods();
+//        clear_mods();
+//        register_code(SE_LALT);
+//        if (temp_mod & MODS_SHIFT_MASK) {
+//          tap_key(SE_P1); tap_key(SE_P5); tap_key(SE_P3); // Ö
+//        } else {
+//          tap_key(SE_P1); tap_key(SE_P4); tap_key(SE_P8); // ö
+//        }
+//        unregister_code(SE_LALT);
+//        return false;
+//      }
+//      break;
+//  }
+//  return true;
+//}
