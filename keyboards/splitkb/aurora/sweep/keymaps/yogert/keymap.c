de6a3c80733bf7ec873875ed2471a242ed0738a8
RodrigueBonnevie <bonnevie@kth.se>
2023-03-30 14:03:33 +0200

    added shortcut for alt + f4

--- keyboards/splitkb/aurora/sweep/keymaps/yogert/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/yogert/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
       KC_RGUI, KC_RCTL, KC_RALT,  KC_TAB, KC_RSFT,                      KC_RSFT,  KC_TAB, KC_RALT, KC_RCTL, KC_RGUI,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,MO(F_KEYS),
+      XXXXXXX, XXXXXXX, XXXXXXX,LALT(KC_F4),XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,MO(F_KEYS),
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX,DF(QWER),DF(COLE),DF(GAME),
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
