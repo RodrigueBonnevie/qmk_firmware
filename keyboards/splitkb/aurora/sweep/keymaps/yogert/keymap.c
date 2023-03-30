991366c7bc0faa678c40e450e00a0cc2d2a80ccd
RodrigueBonnevie <bonnevie@kth.se>
2023-03-30 10:27:54 +0200

    added colemak as default, added combo for ctrl + backspace and modified the f-keys

--- keyboards/splitkb/aurora/sweep/keymaps/yogert/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/yogert/keymap.c
@@ -3,8 +3,8 @@
 
 
 enum ferris_layers {
-  QWER, // QWERTY base layout
   COLE, // Colemak base layout
+  QWER, // QWERTY base layout
   GAME, // For gaming
   NUM,
   SYM,
@@ -17,17 +17,6 @@ enum ferris_layers {
 // TODO: See if you can move the gaming layer to the next to last one
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [QWER] = LAYOUT(
-  //,--------------------------------------------.                    ,--------------------------------------------.
-         SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,
-  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-         SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_QUOT,
-  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-         SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,                         SE_N,    SE_M, SE_COMM,  SE_DOT, SE_SLSH,
-  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-                                          MO(SYM),  KC_SPC,    KC_LSFT, MO(NUM)
-                                      //`-----------------'  `-----------------'
-  ),
   [COLE] = LAYOUT(
   //,--------------------------------------------.                    ,--------------------------------------------.
          SE_Q,    SE_W,    SE_F,    SE_P,    SE_B,                         SE_J,    SE_L,    SE_U,    SE_Y, SE_QUOT,
@@ -40,6 +29,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`-----------------'  `-----------------'
   ),
 
+  [QWER] = LAYOUT(
+  //,--------------------------------------------.                    ,--------------------------------------------.
+         SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+         SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_QUOT,
+  //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
+         SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,                         SE_N,    SE_M, SE_COMM,  SE_DOT, SE_SLSH,
+  //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
+                                          MO(SYM),  KC_SPC,    KC_LSFT, MO(NUM)
+                                      //`-----------------'  `-----------------'
+  ),
+  
   [GAME] = LAYOUT( // For gaming
   //,--------------------------------------------.                    ,--------------------------------------------.
       KC_TAB,     SE_Q,    SE_W,    SE_E,    SE_R,                         SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,
@@ -80,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
       KC_RGUI, KC_RCTL, KC_RALT,  KC_TAB, KC_RSFT,                      KC_RSFT,  KC_TAB, KC_RALT, KC_RCTL, KC_RGUI,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,MO(F_KEYS),
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   MO(F_KEYS), XXXXXXX,DF(QWER),DF(COLE),DF(GAME),
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX,DF(QWER),DF(COLE),DF(GAME),
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
                                           _______,  KC_SPC,    KC_LSFT, _______
                                       //`-----------------'  `-----------------'
@@ -90,11 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [F_KEYS] = LAYOUT(
   //,--------------------------------------------.                    ,--------------------------------------------.
-      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      KC_PSCR,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_PSCR,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F10,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
                                           _______,  KC_SPC,    KC_LSFT, _______
                                       //`-----------------'  `-----------------'
@@ -118,15 +119,17 @@ const uint16_t PROGMEM combo_escape[] = {SE_W, SE_E,    SE_R, COMBO_END};
 const uint16_t PROGMEM combo_enter[]  = {SE_J, SE_K,    SE_L, COMBO_END};
 const uint16_t PROGMEM combo_delete[] = {SE_M, SE_COMM,       COMBO_END};
 // Colemak combos
-const uint16_t PROGMEM combo_colemak_escape[] = {SE_W, SE_F,    SE_P, COMBO_END};
-const uint16_t PROGMEM combo_colemak_enter[]  = {SE_N, SE_E,    SE_I, COMBO_END};
-const uint16_t PROGMEM combo_colemak_delete[] = {SE_H, SE_COMM,       COMBO_END};
+const uint16_t PROGMEM combo_colemak_escape[] =         {SE_W, SE_F,    SE_P,   COMBO_END};
+const uint16_t PROGMEM combo_colemak_enter[]  =         {SE_N, SE_E,    SE_I,   COMBO_END};
+const uint16_t PROGMEM combo_colemak_ctrl_backspace[] = {SE_H, SE_COMM,         COMBO_END};
+const uint16_t PROGMEM combo_colemak_delete[] =         {SE_H, SE_COMM, SE_DOT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_escape,     KC_ESC),
   COMBO(combo_enter,      KC_ENT),
   COMBO(combo_delete,     KC_DEL),
-  COMBO(combo_colemak_escape,     KC_ESC),
-  COMBO(combo_colemak_enter,      KC_ENT),
-  COMBO(combo_colemak_delete,     KC_DEL),
+  COMBO(combo_colemak_escape,         KC_ESC),
+  COMBO(combo_colemak_enter,          KC_ENT),
+  COMBO(combo_colemak_ctrl_backspace, LCTL(KC_BSPC)),
+  COMBO(combo_colemak_delete,         KC_DEL) 
 };
